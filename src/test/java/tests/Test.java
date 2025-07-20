981885495e88e893fa89a16fa4ace5a70e020608
harithamanikkom2012@gmail.com <harithamanikkom2012@gmail.com>
2025-07-20 10:54:35 +0530

    Orange HRM Login Function

---
+++ src/test/java/tests/Test.java
@@ -0,0 +1,30 @@
+package tests;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+
+class Test
+{
+	
+public static void main(String[] args) {
+	
+	WebDriver driver=new FirefoxDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
+	driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	driver.findElement(By.name("username")).sendKeys("Admin");
+	driver.findElement(By.name("password")).sendKeys("admin123");
+	driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+	Assert.assertEquals(driver.getTitle(), "OrangeHRM");
+	driver.quit();
+	
+	
+	
+}
+	
+}
